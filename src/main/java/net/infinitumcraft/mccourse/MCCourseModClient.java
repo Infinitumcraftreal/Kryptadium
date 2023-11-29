22137041210efacfbd29178a88109cc6c502d67c
infi <infinitumcraft@outlook.com>
2023-11-29 22:23:10 +0100

    Added Blocks (If Problems Back Up)

--- src/main/java/net/infinitumcraft/mccourse/MCCourseModClient.java
+++ src/main/java/net/infinitumcraft/mccourse/MCCourseModClient.java
@@ -1,10 +1,14 @@
 package net.infinitumcraft.mccourse;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.infinitumcraft.mccourse.block.ModBlocks;
+import net.minecraft.client.render.RenderLayer;
 
 public class MCCourseModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_TRAPDOOR, RenderLayer.getCutout());
     }
 }
--- src/main/java/net/infinitumcraft/mccourse/item/ModItemGroup.java
+++ src/main/java/net/infinitumcraft/mccourse/item/ModItemGroup.java
@@ -38,6 +38,11 @@ public class ModItemGroup {
                         entries.add(ModBlocks.PINK_GARNET_SLAB);
                         entries.add(ModBlocks.PINK_GARNET_BUTTON);
                         entries.add(ModBlocks.PINK_GARNET_PRESSURE_PLATE);
+                        entries.add(ModBlocks.PINK_GARNET_FENCE);
+                        entries.add(ModBlocks.PINK_GARNET_FENCE_GATE);
+                        entries.add(ModBlocks.PINK_GARNET_WALL);
+                        entries.add(ModBlocks.PINK_GARNET_DOOR);
+                        entries.add(ModBlocks.PINK_GARNET_TRAPDOOR);
 
                 }).build());
 
