57ee5158e4565a1d309e9ba97be0dffd791f901b
infi <infinitumcraft@outlook.com>
2024-08-04 19:20:43 +0200

    Kryptaxo Ai in testing

--- src/main/java/net/infinitumcraft/kryptadium/entity/custom/KryptaxoEntity.java
+++ src/main/java/net/infinitumcraft/kryptadium/entity/custom/KryptaxoEntity.java
@@ -1,12 +1,20 @@
 package net.infinitumcraft.kryptadium.entity.custom;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.mojang.datafixers.util.Pair;
 import net.fabricmc.fabric.impl.resource.loader.FabricModResourcePack;
 import net.infinitumcraft.kryptadium.entity.ModEntities;
 import net.infinitumcraft.kryptadium.sound.ModSounds;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.brain.task.PanicTask;
-import net.minecraft.entity.ai.brain.task.WalkTowardsWaterTask;
+import net.minecraft.entity.ai.brain.Activity;
+import net.minecraft.entity.ai.brain.Brain;
+import net.minecraft.entity.ai.brain.MemoryModuleState;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.ai.brain.task.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -21,6 +29,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
@@ -30,16 +39,16 @@ import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInst
 import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 
-public class KryptaxoEntity extends AnimalEntity implements GeoEntity {
+public class KryptaxoEntity extends AxolotlEntity implements GeoEntity {
     private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
 
-    public KryptaxoEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+    public KryptaxoEntity(EntityType<? extends AxolotlEntity> entityType, World world) {
         super(entityType, world);
     }
 
     public static DefaultAttributeContainer.Builder setAttributes() {
-        return AnimalEntity.createMobAttributes()
-            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 3)
+        return AxolotlEntity.createMobAttributes()
+            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1)
             .add(EntityAttributes.GENERIC_MAX_HEALTH, 14)
             .add(EntityAttributes.GENERIC_ARMOR, 0)
             .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2)
@@ -54,9 +63,7 @@ public class KryptaxoEntity extends AnimalEntity implements GeoEntity {
     }
 
     protected void initGoals() {
-        this.goalSelector.add(1, new SwimGoal(this));
-        this.goalSelector.add(2, new MeleeAttackGoal(this, 3f, false));
-
+        this.goalSelector.add(2, new MeleeAttackGoal(this, 1.2f, false));
         this.goalSelector.add(4, new LookAroundGoal(this));
 
         this.goalSelector.add(2, new ActiveTargetGoal<>(this, ElderGuardianEntity.class, true));
@@ -73,9 +80,11 @@ public class KryptaxoEntity extends AnimalEntity implements GeoEntity {
 
     }
 
+
+
     @Nullable
     @Override
-    public AnimalEntity createChild(ServerWorld world, PassiveEntity entity) {
+    public AxolotlEntity createChild(ServerWorld world, PassiveEntity entity) {
         return ModEntities.KRYPTAXO.create(world);
     }
 
